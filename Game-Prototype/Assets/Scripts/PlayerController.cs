898f6df185f62a71d0940780c83412b3baef0e88
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2023-05-18 15:51:09 +1000

    Squashed commit of the following:
    commit 1073649f7cc9c5a1e0a33947779848dd536e9958
    Author: darcyBeckman <69617028+darcyBeckman@users.noreply.github.com>
    Date:   Tue May 16 17:53:19 2023 +1000
    
        Player controller (#3)
    
        * Import Assets
    
        * Added CameraFollowPlayer, Player Controller
    
        CameraFollowPlayer follows the player from top down (isometric) view, player controller lets the player control the character with wasd. Character also faces mouse direction
    
        * Saved Sample Scene
    
        * Player controls - Player Animations
    
        - Updated player controls to not need rigid body
        - Added sprint option for player (left shift)
        - Added animations for player controller (movement, attack)
        - Added attack control for left mouse click
        - Added health controller for player
        - Added health bar for player
               - Health bar changes with player
                  health
    
        * Rename SampleScene
    
        Renamed my projects SampleScene so that it wouldn't clash with the projects samplescene
    
        * Merge with main
    
        Merge with main
    
        ---------
    
        Co-authored-by: Darcy Beckman <darcybeckman15@gmail.com>

---
+++ Game-Prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+    private Camera mainCamera;
+    Animator anim;
+    bool isSprinting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+        anim = this.GetComponent<Animator>();
+    }
+
+    void MovementControl()
+    {
+        if (Input.GetKey("left shift"))
+        {
+            speed = 10f;
+            isSprinting = true;
+        }
+        else
+        {
+            speed = 5f;
+            isSprinting = false;
+        }
+
+        if (Input.GetKey("w") && Input.GetKey("a"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunLeft");
+            else
+                anim.SetTrigger("isWalkLeft");
+            transform.position += (Vector3.forward * speed * Time.deltaTime) + (Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("w") && Input.GetKey("d"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunRight");
+            else
+                anim.SetTrigger("isWalkRight");
+            transform.position += (Vector3.forward * speed * Time.deltaTime) + (Vector3.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("s") && Input.GetKey("a"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunBck");
+            else
+                anim.SetTrigger("isWalkBck");
+            transform.position += (Vector3.back * speed * Time.deltaTime) + (Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("s") && Input.GetKey("d"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunBck");
+            else
+                anim.SetTrigger("isWalkBck");
+            transform.position += (Vector3.back * speed * Time.deltaTime) + (Vector3.right * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("d"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunFwd");
+            else
+                anim.SetTrigger("isWalkFwd");
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey("a"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunFwd");
+            else
+                anim.SetTrigger("isWalkFwd");
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey("w"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunFwd");
+            else
+                anim.SetTrigger("isWalkFwd");
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey("s"))
+        {
+            if (isSprinting)
+                anim.SetTrigger("isRunBck");
+            else
+                anim.SetTrigger("isWalkBck");
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        else
+        {
+            anim.SetTrigger("isIdle");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementControl();
+
+        if (Input.GetMouseButton(0))
+        {
+            anim.Play("MeeleeAttack_TwoHanded");
+
+        }
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+    }
+}
